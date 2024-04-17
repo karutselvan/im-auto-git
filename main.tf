0fedbe0c3ed218f9db613427cea1b3ab7f3f9f63
karutselvan <114212898+karutselvan@users.noreply.github.com>
2024-04-17 09:13:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,15 @@ resource "google_compute_network" "vpc_network" {
 output "vpc_id"{
   value = google_compute_network.vpc_network.id
 }
+
+module "im-workspace" {
+ source = "terraform-google-modules/bootstrap/google//modules/im_cloudbuild_workspace"
+ version = "~> 7.0"
+
+ project_id = "karutselvan-dm-test"
+ deployment_id = "imgit"
+ im_deployment_repo_uri = "github.com/karutselvan/im-auto-git"
+ im_deployment_ref = "main"
+ github_app_installation_id = "49531130"
+ github_personal_access_token = "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"
+}
